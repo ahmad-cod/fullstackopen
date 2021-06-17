e6a775a9101c584d07a3cb7da8ea9847a2c4b6cd
aroyehunahmad <ahmadadewale800@gmail.com>
2021-06-17 23:27:39 +0100

    initial commit for anecdote app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
-import React, { useState } from 'react';
-import Button from './components/Button';
-import Display from './components/Display';
-import Statistics from './components/Statistics';
+import React, { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0)
-  
-  const handleGood = () => {
-    setGood(good + 1)
-    setAll(all + 1)
-  };
-  const handleNeutral = () => {
-    setNeutral(neutral + 1)
-    setAll(all + 1)
-  };
-  const handleBad = () => {
-    setBad(bad + 1)
-    setAll(all + 1)
-  };
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(Array(anecdotes.length).fill(0).map(i => 0));
+  const [mostVote, setMostVote] = useState(' ');
+  const [mostVoteIndex, setMostVoteIndex] = useState(0);
 
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+  const handleVoteClick = () => {
+    console.log('entered vote', vote);
+    const copy = [...vote];
+    copy[selected] += 1;
+    handleMostVote()
+    return setVote(() => copy)
+  }
+  const handleMostVote = () => {
+    const max = Math.max(...vote);
+    const maxIndex = vote.findIndex(el => el === max)
+    setMostVote(() => anecdotes[maxIndex]) 
+    setMostVoteIndex(() => maxIndex)
+  }
+  console.log(mostVoteIndex)
   return (
-    <React.Fragment>
-      <Display />
-      <Button handleClick={handleGood} text="Good" value={good}/>
-      <Button handleClick={handleNeutral} text="Neutral" value={neutral}/>
-      <Button handleClick={handleBad} text="Bad" value={bad}/>
-      <Statistics values={{good, neutral, bad, all}} />
-    </React.Fragment>
+    <>
+      <div>
+        <h1>Anecdote of the day</h1>
+        {anecdotes[selected]}.  has {vote[selected]} votes
+      </div>
+      <div>
+        <button onClick={handleVoteClick}>Vote</button>
+        <button onClick={handleClick}>next anecdote</button>
+      </div>
+      <div>
+        <h1>Anecdote with most votes</h1>
+        {mostVote}.  has {vote[mostVoteIndex]} votes
+      </div>
+    </>
   )
 }
 
