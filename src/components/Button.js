eef25e52221cd8af3f2a17db1a49a58a0b7741e1
aroyehunahmad <ahmadadewale800@gmail.com>
2021-06-17 23:30:43 +0100

    deleted button and statistic js files since they are unneeded

--- src/components/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Button = ({ text, value, handleClick }) => {
-    
-    return (<button onClick={handleClick}>{text} {value}</button>)
-}
-
-export default Button
