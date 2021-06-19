9faa3fb1477985268c45584701ea9abac2b170b3
aroyehunahmad <ahmadadewale800@gmail.com>
2021-06-20 00:11:18 +0100

    handled showing of individual country

--- src/App.js
+++ src/App.js
@@ -1,50 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import Filter from './components/Filter';
-import Form from './components/Form';
-import Persons from './components/Persons';
+import Countries from './components/Countries';
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [ newName, setNewName ] = useState('');
-  const [ newNumber, setNewNumber ] = useState('');
+  const [countries, setCountries] = useState([]);
   const [ filtered, setFiltered ] = useState([]);
 
   useEffect(() => {
     console.log('effect')
-    axios.get('http://localhost:3001/persons')
+    axios.get('https://restcountries.eu/rest/v2/all')
         .then(response => {
-          console.log(response)
-          setPersons(response.data)
+          console.log(response.data)
+          setCountries(response.data)
         }) 
   
   }, [])
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if(!newName) return alert('please input a name')
-    if(persons.find(person => person.name === newName)) return alert(`${newName} is already added to phonebook`)
-    const newPersons = [...persons, {name: newName, number: newNumber}]
-    setPersons(newPersons)
-  }
   const handleChange = (e) => {
-    if(e.target.id === 'name') return setNewName(() => e.target.value)
-    if(e.target.id === 'number') return setNewNumber(() => e.target.value)
-    if(e.target.id === 'filter') {
-      let filtered = persons.filter(person => person.name.includes(e.target.value))
-      console.log(filtered);
-      setFiltered(() => filtered)
-    }
+    if(!e.target.value) return setFiltered([]);
+    let input = new RegExp(e.target.value,'i')
+    
+    let filtered = countries.filter(country => input.test(country.name))
+    console.log(filtered);
+    setFiltered(filtered)
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Rest Countries</h2>
       <Filter handleChange={handleChange} />
-      <Form handleChange={handleChange} handleSubmit={handleSubmit} />
-      <h2>Numbers</h2>
-      <Persons persons={persons} filtered={filtered} />
-      ...<div>debug: {newName}</div>
+      <Countries filtered={filtered} />
     </div>
   )
 }
