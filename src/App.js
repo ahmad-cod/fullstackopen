23f539a8dcb4af457eac4510c1e5e99acc01410e
aroyehunahmad <ahmadadewale800@gmail.com>
2021-06-19 18:28:53 +0100

    stored and fetched initial data from db.json

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import Filter from './components/Filter';
 import Form from './components/Form';
 import Persons from './components/Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ filtered, setFiltered ] = useState([]);
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/persons')
+        .then(response => {
+          console.log(response)
+          setPersons(response.data)
+        }) 
+  
+  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault();
